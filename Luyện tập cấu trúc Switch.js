13dba1265aa9feb69a6d68b78793432333b3abef
datgiaosu2k1 <datgiaosu2k1@gmail.com>
2025-07-28 14:01:02 +0900

    upload BTVN session 11-03

---
+++ Luyện tập cấu trúc Switch.js
@@ -0,0 +1,38 @@
+let number = +prompt("nhập một số nguyên");
+switch(number){
+    case 0:
+        alert("số không");
+        break;
+    case 1:
+        alert("số một");
+        break;
+    case 2:
+        alert("số hai");
+        break;
+    case 3:
+        alert("số ba");
+        break;
+    case 4:
+        alert("số bốn");
+        break;
+    case 5:
+        alert("số năm");
+        alert("số năm");
+        break;
+    case 6:
+        alert("số 6");
+        break;
+    case 7:
+        alert("số bảy");
+        break;
+    case 8:
+        alert("số tám");
+        break;
+    case 9:
+        alert("số chín");
+        break;
+        default:
+        alert(" mời bạn nhập số từ 0-9");
+        break;
+    
+}
